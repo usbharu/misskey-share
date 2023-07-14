b616d84b4b03b34881a207eb5dcad692158c0c6d
usbharu <64310155+usbharu@users.noreply.github.com>
2023-07-14 15:25:04 +0900

    不要なファイルを削除

--- content.js
+++
@@ -1,8 +0,0 @@
-const s = document.createElement("script");
-s.src = chrome.runtime.getURL("inject.js");
-(document.head || document.documentElement).appendChild(s);
-s.parentNode.removeChild(s);
-
-window.addEventListener("message",function (event) {
-
-});
